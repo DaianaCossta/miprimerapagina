0e3e370254d403d4e83b3b9403443aab62a8d1ab
DaianaCossta <daiianacosta14@hotmail.com>
2024-02-19 18:13:14 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre = "Daiana";
+//console.log(nombre);
+
+let contenidoTitulo = "Nombre";
+let titulo = document.querySelector (".logo .fuente");
+titulo.innerHTML = contenidoTitulo;
+
+let textoTitulo = titulo.innerText;
+console.log (textoTitulo);
+
+//if (textoTitulo == "Nombre") {
+  //  titulo.innerHTML = "Otro";
+//} else {
+  //  console.log("no se cumple");
+//}
+
+//let nombre1 = "Dai";
+//let ciudad = "Paysandu";
+//let gusto = "bebida";
+
+//let parrafo = document.querySelector(".parrafo2");
+
+//function cambiarTexto (nombre1, ciudad, gusto){
+  //  let contenido = `¡Bienvenido a mi página! Me llamo ${nombre1}, soy de ${ciudad}, Uruguay y me apasiona la ${bebida}, te espero para conectar a través de la comida.`;
+    //return contenido;
+//}
+
+//parrafo.innerText = cambiarTexto ("Dai", ciudad, gusto);
